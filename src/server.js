287b0ecd8a87621a23e5a94055ede5496d77ca23
Gloria nazer <glorianaa@hotmail.co.uk>
2021-04-11 16:49:42 +0100

    nodemon added and set up

---
+++ src/server.js
@@ -0,0 +1,7 @@
+// in order to make a web server this is the bone of it  up collum 7 
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => { 
+    console.log("listening on port 5000");
+});
